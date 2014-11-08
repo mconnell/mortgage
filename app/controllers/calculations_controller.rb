55fa1264b93741ab57c8dba540ebb4a4d0b167e8
Mark Connell <mark@markconnell.co.uk>
2014-11-08 19:37:12 +0000

    up to 200k

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -3,7 +3,7 @@ class CalculationsController < ApplicationController
   def monthly
     interest_rates = [5,8]
     @deposit       = params[:deposit_percent].present? ? params[:deposit_percent].to_i :  10
-    prices         = [55000.0, 70000.0, 85000.0, 100000.0, 115000.0, 130000.0, 145000.0, 160000.0, 175000.0]
+    prices         = [55000.0, 70000.0, 85000.0, 100000.0, 115000.0, 130000.0, 145000.0, 160000.0, 175000.0, 190000.0, 205000.0]
 
     @results = prices.map do |price|
       interest_rates.map do |interest_rate|
