56d243552c4c4b9b1ba6eea722612c5b6f0aada3
Mark Connell <mark@markconnell.co.uk>
2014-11-08 19:29:55 +0000

    adjustable deposit

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -2,12 +2,12 @@ class CalculationsController < ApplicationController
 
   def monthly
     interest_rates = [5,8]
-    deposit        = 10
+    @deposit       = params[:deposit_percent].present? ? params[:deposit_percent].to_i :  10
     prices         = [55000.0, 70000.0, 85000.0, 100000.0, 115000.0, 130000.0, 145000.0, 160000.0, 175000.0]
 
     @results = prices.map do |price|
       interest_rates.map do |interest_rate|
-        Calculator.new(price: price, annual_interest_rate: interest_rate, deposit_percent: 10)
+        Calculator.new(price: price, annual_interest_rate: interest_rate, deposit_percent: @deposit)
       end
     end
   end
